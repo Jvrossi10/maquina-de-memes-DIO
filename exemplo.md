f40669138cd8f90cc0e8e2f0be3c146f7a55fce4
Jvrossi10 <Jvrossi10@gmail.com>
2022-11-18 22:25:19 -0300

    atualizando repositorio

---
+++ exemplo.md
@@ -0,0 +1,24 @@
+ROTINA 
+
+# Café da manha 
+# Toma banho
+# Escova os dentes
+# Troca de roupa 
+
+
+```js
+    function fazerTorradas(){}
+    function tomarBanho(){}
+    function escovarOsDentes(){}
+    function trocarDeRoupa(){}
+```
+
+JOB DO RENAN
+Rotina(s): Listar conteúdos do DIO PRO e exibir para o usuário
+
+```js 
+    function openModal(){}
+    function listPrograms(){}
+    function getImages(){}
+    function renderView(){}
+```
---
+++ javascript/scripts.js
@@ -0,0 +1,46 @@
+async function mapImageList(){
+    const memesObject = [
+        {
+            "name": "chapolin",
+            "path": "pictures/chapolin.jpg"
+        }, 
+        {
+            "name": "chloe",
+            "path": "pictures/chloe.jpg"
+        },
+        {
+            "name": "funny-cat1",
+            "path": "pictures/funny-cat1.png"
+        },
+        {
+            "name": "funny-cat2",
+            "path": "pictures/funny-cat2.png"
+        }
+    ]
+    return memesObject
+}
+
+async function createGallery(imageList){
+    const memeSelector = document.querySelector("#meme-list")
+
+    imageList.forEach(picture => {
+        let newOption = document.createElement("option")
+        newOption.text = picture.name.toUpperCase()
+        newOption.value = picture.path
+        memeSelector.appendChild(newOption)
+    });
+} 
+
+async function changeMemePicture(photo){
+    let displayImage = document.querySelector("#display-image")
+    displayImage.style.backgroundImage =`url('${photo}')`
+}
+
+async function main(){
+    const memesImageList = await mapImageList()
+
+    await createGallery(memesImageList)
+    await changeMemePicture(memesImageList[3].path)
+} 
+
+main()
